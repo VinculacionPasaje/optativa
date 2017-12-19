9201dad22b6a2246fc28da65077cfa8667c5b47d
Fernando Cas <grupoturismopasaje@gmail.com>
2017-12-19 00:04:21 -0500

    inicio

--- cinema3/cinema3/settings.py
+++ cinema3/cinema3/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.actors',
+    'app.categories',
+    'app.languages',
+    'app.movies',
+    'app.servers',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
